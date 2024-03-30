4d5a9107871d6022b028b5ef7a0c78ad6798a287
azantek <andrea.d.zantek@lawrence.edu>
2024-03-30 01:12:50 +0000

    Fixed dynamic tab... working on uploading

--- app.R
+++ app.R
@@ -11,6 +11,13 @@ library(fGarch)
 library(shiny)
 ################################################################################
 
+library(shiny)
+library(ggplot2)
+library(readr)
+
+library(shiny)
+library(ggplot2)
+
 # user interface
 ui <- fluidPage(
   # Application title
@@ -77,42 +84,92 @@ ui <- fluidPage(
                       plotOutput("hist_dynamic")),
                column(6,
                       plotOutput("boxplot_dynamic")),
-             
+               
+             )
+    ),
+    
+    tabPanel("Upload File",
+             fluidRow(
+               fileInput("file", "Upload a file"),
+               column(3, plotOutput("uploaded_hist")),
+               column(3, plotOutput("uploaded_boxplot")),
+               column(6, tableOutput("uploaded_table"))
              )
     )
   )
 )
 
-
-
 # server function
 server <- function(input, output) {
+  
+  uploaded_data <- reactive({
+    req(input$file)
+    df <- read.csv(input$file$datapath, header = TRUE)
+    return(df)
+  })
+  
+  output$uploaded_hist <- renderPlot({
+    if (!is.null(uploaded_data())) {
+      ggplot(data = uploaded_data(), aes(x = value)) +
+        geom_histogram(fill = "lightblue", color = "black", bins = 30) +
+        labs(x = "Value", y = "Frequency", title = "Uploaded Histogram") +
+        geom_vline(mapping = aes(xintercept = mean(uploaded_data()$value), color = "Mean")) +
+        geom_vline(mapping = aes(xintercept = median(uploaded_data()$value), color = "Median")) +
+        scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
+        theme(legend.position = "right")
+    }
+  })
+  
+  output$uploaded_boxplot <- renderPlot({
+    if (!is.null(uploaded_data())) {
+      ggplot(data = uploaded_data(), aes(y = value)) +
+        geom_boxplot(fill = "lightblue", color = "black") +
+        labs(y = "Value", x = "", title = "Uploaded Boxplot") +
+        geom_hline(yintercept = mean(uploaded_data()$value), color = "red") +
+        geom_hline(yintercept = median(uploaded_data()$value), color = "blue") +
+        theme(legend.position = "none")
+    }
+  })
+  
+  output$uploaded_table <- renderTable({
+    if (!is.null(uploaded_data())) {
+      summary(uploaded_data()$value)
+    }
+  })
   
   df <- reactive({
     if(input$shape == "Symmetric") {
       val <- rnorm(1000, mean = input$mean_value, sd = input$sd_value)
       df <- data.frame(value = val)
     } else if (input$shape == "Positively Skewed") {
-      # Generate 900 normally distributed values and 100 values between the mean and 100 points larger than the mean
       val_right <- c(rnorm(900, mean = input$mean_value, sd = input$sd_value),
-                     runif(100, min = input$mean_value, max = input$mean_value+100))
+                     runif(100, min = input$mean_value, max = input$mean_value + 100))
       df <- data.frame(value = val_right)
     } else {
-      # Generate 900 normally distributed values and 100 values between the 100 minus the mean and the mean
-      
       val_left <- c(rnorm(900, mean = input$mean_value, sd = input$sd_value),
-                    runif(100, min = input$mean_value-100, max = input$mean_value))
+                    runif(100, min = input$mean_value - 100, max = input$mean_value))
       df <- data.frame(value = val_left)
     }
-    
-    # val_right <- c(val, tail(val[order(val)], 100) + rnorm(100, mean=input$mean_value+10, sd=2))
-    # val_transform <- log10(val); df <- data.frame(val_transform)
     return(df)
   })
   
+  df_dynamic <- reactive({
+    if(input$shape_dynamic == "Symmetric") {
+      val <- rnorm(1000, mean = input$mean_value_dynamic, sd = input$sd_value_dynamic)
+      df <- data.frame(value = val)
+    } else if (input$shape_dynamic == "Positively Skewed") {
+      val_right <- c(rnorm(900, mean = input$mean_value_dynamic, sd = input$sd_value_dynamic),
+                     runif(100, min = input$mean_value_dynamic, max = input$mean_value_dynamic + 100))
+      df <- data.frame(value = val_right)
+    } else {
+      val_left <- c(rnorm(900, mean = input$mean_value_dynamic, sd = input$sd_value_dynamic),
+                    runif(100, min = input$mean_value_dynamic - 100, max = input$mean_value_dynamic))
+      df <- data.frame(value = val_left)
+    }
+    return(df)
+  })
   
-output$hist <- renderPlot({
-    
+  output$hist <- renderPlot({
     ggplot(data = df()) +
       geom_histogram(mapping = aes(x = value, fill = "Histogram"), color = "black", bins = 100) +
       labs(x = "Value", y = "Frequency", title = "Histogram") +
@@ -122,25 +179,21 @@ output$hist <- renderPlot({
       scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
       xlim(c(-100, 100)) +
       theme(legend.position = "right")
-    
   })
-
-output$hist_dynamic <- renderPlot({
   
-  ggplot(data = df()) +
-    geom_histogram(mapping = aes(x = value, fill = "Histogram"), color = "black", bins = 100) +
-    labs(x = "Value", y = "Frequency", title = "Histogram") +
-    geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean"))+
-    geom_vline(mapping = aes(xintercept = median(df()$value), color = "Median")) +
-    scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
-    scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
-    xlim(c(-100, 100)) +
-    theme(legend.position = "right")
-  
-})
+  output$hist_dynamic <- renderPlot({
+    ggplot(data = df_dynamic()) +
+      geom_histogram(mapping = aes(x = value, fill = "Histogram"), color = "black", bins = 100) +
+      labs(x = "Value", y = "Frequency", title = "Histogram") +
+      geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean"))+
+      geom_vline(mapping = aes(xintercept = median(df()$value), color = "Median")) +
+      scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
+      scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
+      xlim(c(-100, 100)) +
+      theme(legend.position = "right")
+  })
   
   output$boxplot <- renderPlot({
-    
     ggplot(data = df()) +
       geom_boxplot(mapping = aes(x = value, fill = "Boxplot"), color = "black") +
       theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
@@ -151,41 +204,21 @@ output$hist_dynamic <- renderPlot({
       scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
       xlim(c(-100, 100)) +
       theme(legend.position = "right")
-    
   })
   
   output$boxplot_dynamic <- renderPlot({
-    
-    ggplot(data = df()) +
+    ggplot(data = df_dynamic()) +
       geom_boxplot(mapping = aes(x = value, fill = "Boxplot"), color = "black") +
       theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
       labs(x = "Value", y = "", title = "Boxplot") +
-      geom_vline(mapping = aes(xintercept = 0, color = "Mean"))+
+      geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean")) + # Calculate mean dynamically
       geom_vline(mapping = aes(xintercept = median(df()$value), color = "Median")) +
       scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
       scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
       xlim(c(-100, 100)) +
       theme(legend.position = "right")
-
-  
   })
-  
-  
-  output$table <- renderTable({
-    
-    res <- favstats(~ value, data=df())
-    ftable <- data.frame(Summary = c("min", "Q1", "median", "Q3", "max", "IQR", "range"),
-                         Values = c(round(res$min,3), round(res$Q1,3), round(res$median,3), 
-                                    round(res$Q3,3), round(res$max,3), 
-                                    round(res$Q3,3) - round(res$Q1,3),
-                                    round(res$max,3) - round(res$min,3)))
-    
-    ftable
-  })
-  
 }
-
 
 # Run the application
 shinyApp(ui = ui, server = server)
-################################################################################
