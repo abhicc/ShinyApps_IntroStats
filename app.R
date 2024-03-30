6a0f08235811a73a521af50b1b2b0a5b5cd1f454
azantek <andrea.d.zantek@lawrence.edu>
2024-03-30 01:55:49 +0000

    upload file working

--- app.R
+++ app.R
@@ -15,11 +15,9 @@ library(shiny)
 library(ggplot2)
 library(readr)
 
-library(shiny)
-library(ggplot2)
-
-# user interface
+# User interface
 ui <- fluidPage(
+  
   # Application title
   titlePanel("Summarizing Numerical Data: Visualizing Mean and Median"),
   
@@ -91,50 +89,68 @@ ui <- fluidPage(
     tabPanel("Upload File",
              fluidRow(
                fileInput("file", "Upload a file"),
+               column(3, selectInput("var", "Select a variable", "")),
                column(3, plotOutput("uploaded_hist")),
                column(3, plotOutput("uploaded_boxplot")),
-               column(6, tableOutput("uploaded_table"))
+               column(3, tableOutput("uploaded_table"))
              )
     )
   )
 )
 
-# server function
-server <- function(input, output) {
+
+# Define server logic
+# Define server logic
+server <- function(input, output, session) {
   
   uploaded_data <- reactive({
     req(input$file)
     df <- read.csv(input$file$datapath, header = TRUE)
+    updateSelectInput(session, "var", choices = names(df))
     return(df)
   })
   
   output$uploaded_hist <- renderPlot({
-    if (!is.null(uploaded_data())) {
-      ggplot(data = uploaded_data(), aes(x = value)) +
-        geom_histogram(fill = "lightblue", color = "black", bins = 30) +
-        labs(x = "Value", y = "Frequency", title = "Uploaded Histogram") +
-        geom_vline(mapping = aes(xintercept = mean(uploaded_data()$value), color = "Mean")) +
-        geom_vline(mapping = aes(xintercept = median(uploaded_data()$value), color = "Median")) +
-        scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
-        theme(legend.position = "right")
-    }
+    req(uploaded_data(), input$var)
+    ggplot(data = uploaded_data(), aes_string(x = input$var)) +
+      geom_histogram(mapping = aes(x = .data[[input$var]], fill = "Histogram"), color = "black", bins = 30) +
+      labs(x = "Value", y = "Frequency", title = "Uploaded Histogram") +
+      geom_vline(mapping = aes(xintercept = mean(uploaded_data()[[input$var]]), color = "Mean")) +
+      geom_vline(mapping = aes(xintercept = median(uploaded_data()[[input$var]]), color = "Median")) +
+      scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
+      scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
+      theme(legend.position = "right")
   })
   
   output$uploaded_boxplot <- renderPlot({
-    if (!is.null(uploaded_data())) {
-      ggplot(data = uploaded_data(), aes(y = value)) +
-        geom_boxplot(fill = "lightblue", color = "black") +
-        labs(y = "Value", x = "", title = "Uploaded Boxplot") +
-        geom_hline(yintercept = mean(uploaded_data()$value), color = "red") +
-        geom_hline(yintercept = median(uploaded_data()$value), color = "blue") +
-        theme(legend.position = "none")
-    }
+    req(uploaded_data(), input$var)
+    ggplot(data = uploaded_data(), aes_string(x = input$var)) +
+      geom_boxplot(color = "black", fill = "lightgreen", bins = 30) +
+      geom_vline(mapping = aes(xintercept = mean(uploaded_data()[[input$var]]), color = "Mean")) +
+      geom_vline(mapping = aes(xintercept = median(uploaded_data()[[input$var]]), color = "Median")) +
+      scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
+      scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
+      labs(x = "Value", y = "input$var", title = "Uploaded Boxplot") +
+      theme(legend.position = "right")
   })
-  
   output$uploaded_table <- renderTable({
-    if (!is.null(uploaded_data())) {
-      summary(uploaded_data()$value)
-    }
+    req(uploaded_data(), input$var)
+    
+    # Assuming df() is your uploaded data
+    data_summary <- summary(uploaded_data()[[input$var]], na.rm = TRUE)
+    
+    # Calculate IQR and range
+    data_iqr <- IQR(uploaded_data()[[input$var]], na.rm = TRUE)
+    data_range <- diff(range(uploaded_data()[[input$var]], na.rm = TRUE))
+    
+    # Create a data frame with summary statistics
+    atable <- data.frame(Summary = c("min", "Q1", "median", "Q3", "max", "IQR", "range"),
+                         Values = c(data_summary["Min."], data_summary["1st Qu."], data_summary["Median"],
+                                    data_summary["3rd Qu."], data_summary["Max."],
+                                    data_iqr,
+                                    data_range))
+    
+    atable
   })
   
   df <- reactive({
@@ -173,7 +189,7 @@ server <- function(input, output) {
     ggplot(data = df()) +
       geom_histogram(mapping = aes(x = value, fill = "Histogram"), color = "black", bins = 100) +
       labs(x = "Value", y = "Frequency", title = "Histogram") +
-      geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean"))+
+      geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean")) +
       geom_vline(mapping = aes(xintercept = median(df()$value), color = "Median")) +
       scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
       scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
@@ -185,7 +201,7 @@ server <- function(input, output) {
     ggplot(data = df_dynamic()) +
       geom_histogram(mapping = aes(x = value, fill = "Histogram"), color = "black", bins = 100) +
       labs(x = "Value", y = "Frequency", title = "Histogram") +
-      geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean"))+
+      geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean")) +
       geom_vline(mapping = aes(xintercept = median(df()$value), color = "Median")) +
       scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
       scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
@@ -198,7 +214,7 @@ server <- function(input, output) {
       geom_boxplot(mapping = aes(x = value, fill = "Boxplot"), color = "black") +
       theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
       labs(x = "Value", y = "", title = "Boxplot") +
-      geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean"))+
+      geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean")) +
       geom_vline(mapping = aes(xintercept = median(df()$value), color = "Median")) +
       scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
       scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
@@ -218,7 +234,20 @@ server <- function(input, output) {
       xlim(c(-100, 100)) +
       theme(legend.position = "right")
   })
+  output$table <- renderTable({
+    
+    res <- favstats(~ value, data=df())
+    ftable <- data.frame(Summary = c("min", "Q1", "median", "Q3", "max", "IQR", "range"),
+                         Values = c(round(res$min,3), round(res$Q1,3), round(res$median,3), 
+                                    round(res$Q3,3), round(res$max,3), 
+                                    round(res$Q3,3) - round(res$Q1,3),
+                                    round(res$max,3) - round(res$min,3)))
+    
+    ftable
+  })
+  
 }
+
 
 # Run the application
 shinyApp(ui = ui, server = server)
