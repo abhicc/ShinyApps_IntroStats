ce1453a78345f6b246dffc9f33f85850ba298e92
ninada25 <austrian@lawrence.edu>
2024-04-02 04:15:23 +0000

    changed colors, added mean to summary table, fixed mean and median lines, removed unnecc sliders

--- app.R
+++ app.R
@@ -22,29 +22,15 @@ ui <- fluidPage(
     tabPanel("Fixed Mean and SD",
              fluidRow(
                column(3,
-                      sliderInput(inputId = "mean_value",
-                                  label = "Input a mean value",
-                                  min = -0,
-                                  max = 0,
-                                  value = 0,
-                                  step = 0.1)),
-               column(3,
-                      sliderInput(inputId = "sd_value",
-                                  label = "Input a standard deviation",
-                                  min = 10,
-                                  max = 10,
-                                  value = 10, 
-                                  step = 1)),
-               column(3,
                       selectInput(inputId = "shape",
                                   label = "Select a shape",
                                   choices = c("Symmetric", "Positively Skewed", "Negatively Skewed"),
                                   selected = "Symmetric"))
              ),
              fluidRow(
-               column(5, 
+               column(6, 
                       plotOutput("hist")),
-               column(5,
+               column(6,
                       plotOutput("boxplot")),
                column(2,
                       tableOutput("table"))
@@ -77,8 +63,7 @@ ui <- fluidPage(
                column(6, 
                       plotOutput("hist_dynamic")),
                column(6,
-                      plotOutput("boxplot_dynamic")),
-               
+                      plotOutput("boxplot_dynamic"))
              )
     ),
     
@@ -125,9 +110,9 @@ server <- function(input, output, session) {
       labs(x = "Value", y = "Frequency", title = "Histogram") +
       geom_vline(mapping = aes(xintercept = mean(data[[input$var]]), color = "Mean")) +
       geom_vline(mapping = aes(xintercept = median(data[[input$var]]), color = "Median")) +
-      scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
-      scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
-      theme(legend.position = "right")
+      scale_color_manual("", values = c(Mean = "#D55E00", Median = "#0072B2")) +
+      scale_fill_manual("", values = c("#56B4E9"), guide = FALSE) +
+      theme(legend.position = "none")
   })
   
   output$uploaded_boxplot <- renderPlot({
@@ -137,11 +122,11 @@ server <- function(input, output, session) {
     data <- data[complete.cases(data[[input$var]]) & is.finite(data[[input$var]]), ]
     
     ggplot(data = data, aes_string(x = input$var)) +
-      geom_boxplot(color = "black", fill = "lightgreen") +
+      geom_boxplot(color = "black", fill = "#56B4E9") +
       geom_vline(mapping = aes(xintercept = mean(data[[input$var]]), color = "Mean")) +
       geom_vline(mapping = aes(xintercept = median(data[[input$var]]), color = "Median")) +
-      scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
-      scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
+      scale_color_manual("", values = c(Mean = "#D55E00", Median = "#0072B2")) +
+      scale_fill_manual("", values = c("#56B4E9"), guide = FALSE) +
       labs(x = "Value", y = " ", title = "Boxplot") +
       theme(legend.position = "right")
   })
@@ -156,8 +141,8 @@ server <- function(input, output, session) {
     data_iqr <- IQR(uploaded_data()[[input$var]], na.rm = TRUE)
     data_range <- diff(range(uploaded_data()[[input$var]], na.rm = TRUE))
     
-    atable <- data.frame(Summary = c("min", "Q1", "median","mean", "Q3", "max", "IQR", "range"),
-                         Values = c(data_summary["Min."], data_summary["1st Qu."], data_summary["Median"], data_summary["Mean"],
+    atable <- data.frame(Summary = c("min", "Q1", "mean", "median", "Q3", "max", "IQR", "range"),
+                         Values = c(data_summary["Min."], data_summary["1st Qu."], data_summary["Mean"], data_summary["Median"],
                                     data_summary["3rd Qu."], data_summary["Max."],
                                     data_iqr,
                                     data_range))
@@ -168,21 +153,19 @@ server <- function(input, output, session) {
   # Create dataframe for fixed tab
   df <- reactive({
     if(input$shape == "Symmetric") {
-      val <- rnorm(1000, mean = input$mean_value, sd = input$sd_value)
+      val <- rnorm(1000, mean = 0, sd = 10)
       df <- data.frame(value = val)
     } else if (input$shape == "Positively Skewed") {
       # Generate 900 normally distributed values and 100 values between the mean and 100 points larger than the mean
-      val_right <- c(rnorm(900, mean = input$mean_value, sd = input$sd_value),
-                     runif(100, min = input$mean_value, max = input$mean_value + 100))
+      val_right <- c(rnorm(900, mean = 0, sd = 10), # fix mean to 0 and sd to 10
+                     runif(100, min = 0, max = 100))
       df <- data.frame(value = val_right)
     } else {
       # Generate 900 normally distributed values and 100 values between the 100 minus the mean and the mean
-      val_left <- c(rnorm(900, mean = input$mean_value, sd = input$sd_value),
-                    runif(100, min = input$mean_value - 100, max = input$mean_value))
+      val_left <- c(rnorm(900, mean = 0, sd = 10),
+                    runif(100, min = -100, max = 0))
       df <- data.frame(value = val_left)
     }
-    # val_right <- c(val, tail(val[order(val)], 100) + rnorm(100, mean=input$mean_value+10, sd=2))
-    # val_transform <- log10(val); df <- data.frame(val_transform)
     return(df)
   })
   
@@ -193,11 +176,11 @@ server <- function(input, output, session) {
       df <- data.frame(value = val)
     } else if (input$shape_dynamic == "Positively Skewed") {
       val_right <- c(rnorm(900, mean = input$mean_value_dynamic, sd = input$sd_value_dynamic),
-                     runif(100, min = input$mean_value_dynamic, max = input$mean_value_dynamic + 100))
+                     runif(100, min = input$mean_value_dynamic, max = input$mean_value_dynamic + 100)) # increasing positive skewness
       df <- data.frame(value = val_right)
     } else {
       val_left <- c(rnorm(900, mean = input$mean_value_dynamic, sd = input$sd_value_dynamic),
-                    runif(100, min = input$mean_value_dynamic - 100, max = input$mean_value_dynamic))
+                    runif(100, min = input$mean_value_dynamic - 100, max = input$mean_value_dynamic)) # increasing negative skewness
       df <- data.frame(value = val_left)
     }
     return(df)
@@ -209,8 +192,8 @@ server <- function(input, output, session) {
       labs(x = "Value", y = "Frequency", title = "Histogram") +
       geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean")) +
       geom_vline(mapping = aes(xintercept = median(df()$value), color = "Median")) +
-      scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
-      scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
+      scale_color_manual("", values = c(Mean = "#D55E00", Median = "#0072B2")) +
+      scale_fill_manual("", values = c("#56B4E9"), guide = FALSE) +
       xlim(c(-100, 100)) +
       theme(legend.position = "right")
   })
@@ -219,10 +202,10 @@ server <- function(input, output, session) {
     ggplot(data = df_dynamic()) +
       geom_histogram(mapping = aes(x = value, fill = "Histogram"), color = "black", bins = 100) +
       labs(x = "Value", y = "Frequency", title = "Histogram") +
-      geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean")) +
-      geom_vline(mapping = aes(xintercept = median(df()$value), color = "Median")) +
-      scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
-      scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
+      geom_vline(mapping = aes(xintercept = mean(df_dynamic()$value), color = "Mean")) +
+      geom_vline(mapping = aes(xintercept = median(df_dynamic()$value), color = "Median")) +
+      scale_color_manual("", values = c(Mean = "#D55E00", Median = "#0072B2")) +
+      scale_fill_manual("", values = c("#56B4E9"), guide = FALSE) +
       xlim(c(-100, 100)) +
       theme(legend.position = "right")
   })
@@ -234,8 +217,8 @@ server <- function(input, output, session) {
       labs(x = "Value", y = "", title = "Boxplot") +
       geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean")) +
       geom_vline(mapping = aes(xintercept = median(df()$value), color = "Median")) +
-      scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
-      scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
+      scale_color_manual("", values = c(Mean = "#D55E00", Median = "#0072B2")) +
+      scale_fill_manual("", values = c("#56B4E9"), guide = FALSE) +
       xlim(c(-100, 100)) +
       theme(legend.position = "right")
   })
@@ -245,18 +228,18 @@ server <- function(input, output, session) {
       geom_boxplot(mapping = aes(x = value, fill = "Boxplot"), color = "black") +
       theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
       labs(x = "Value", y = "", title = "Boxplot") +
-      geom_vline(mapping = aes(xintercept = mean(df()$value), color = "Mean")) + # Calculate mean dynamically
-      geom_vline(mapping = aes(xintercept = median(df()$value), color = "Median")) +
-      scale_color_manual("", values = c(Mean = "red", Median = "blue")) +
-      scale_fill_manual("", values = c("lightgreen"), guide = FALSE) +
+      geom_vline(mapping = aes(xintercept = mean(df_dynamic()$value), color = "Mean")) + # Calculate mean dynamically
+      geom_vline(mapping = aes(xintercept = median(df_dynamic()$value), color = "Median")) +
+      scale_color_manual("", values = c(Mean = "#D55E00", Median = "#0072B2")) +
+      scale_fill_manual("", values = c("#56B4E9"), guide = FALSE) +
       xlim(c(-100, 100)) +
       theme(legend.position = "right")
   })
   output$table <- renderTable({
     
     res <- favstats(~ value, data=df())
-    ftable <- data.frame(Summary = c("min", "Q1", "median", "Q3", "max", "IQR", "range"),
-                         Values = c(round(res$min,3), round(res$Q1,3), round(res$median,3), 
+    ftable <- data.frame(Summary = c("min", "Q1", "mean", "median", "Q3", "max", "IQR", "range"),
+                         Values = c(round(res$min,3), round(res$Q1,3), round(res$mean,3), round(res$median,3), 
                                     round(res$Q3,3), round(res$max,3), 
                                     round(res$Q3,3) - round(res$Q1,3),
                                     round(res$max,3) - round(res$min,3)))
