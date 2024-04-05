e51dc89fa98a03537ad46f225c1f639430b7970a
azantek <andrea.d.zantek@lawrence.edu>
2024-04-05 01:21:44 +0000

    created app

---
+++ Sampling/plumber.R
@@ -0,0 +1,15 @@
+################################################################################
+# This app demonstrates the concept of confidence intervals for various contexts,  
+# specifically the ideas such as confidence intervals are not fixed, they might not 
+# contain the "true" population parameter, how CIs are affected by sample sizes, 
+# the interpretation of CIs.
+################################################################################
+# Required libraries
+library(tidyverse)
+library(mosaic)
+library(shiny)
+################################################################################
+library(shiny)
+library(ggplot2)
+library(psych)
+
