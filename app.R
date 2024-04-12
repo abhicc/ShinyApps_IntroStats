b6ff635dbfd37a66da993b724dc6b7c9d552bd71
azantek <andrea.d.zantek@lawrence.edu>
2024-04-12 22:46:27 +0000

    working!

--- app.R
+++ app.R
@@ -21,8 +21,8 @@ library(fGarch)
 library(shiny)
 
 # UI code
-
 # UI code
+
 ui <- fluidPage(
   titlePanel("Summarizing Numerical Data: Visualizing Mean and Median"),
   tabsetPanel(
@@ -68,24 +68,23 @@ ui <- fluidPage(
     
     tabPanel("Upload File",
              fluidRow(
-               column(6, 
+               column(4, 
                       sidebarPanel(
                         h4("Choose Dataset or Upload a CSV file"),
                         radioButtons("data_choice", "Select Data Source:", 
                                      choices = c( "Upload CSV File", "Preloaded Dataset"),
                                      selected = "Upload CSV File"),
                         conditionalPanel(
-                          condition = "input.data_choice != 'Preloaded Dataset'",
+                          condition = "input.data_choice == 'Upload CSV File'",
                           fileInput("file", "", accept = ".csv")
                         ),
                         uiOutput("dataset_select"),
                         uiOutput("variable_select")
                       )),
-               column(6, 
-                      mainPanel(
-                        plotOutput("upload_plot", width = "100%", height = "400px"),
-                        tableOutput("upload_table")
-                      ))
+               column(8, 
+                      uiOutput("data_plot"),
+                      tableOutput("data_table")
+               )
              )
     )
   )
@@ -119,179 +118,141 @@ server <- function(input, output, session) {
     }
   })
   
-  output$dataset_plot <- renderPlot({
-    req(input$var, input$data_choice, input$dataset)
-    
-    print("Dataset plot rendering started...")
-    
+  output$data_plot <- renderUI({
     if (input$data_choice == "Preloaded Dataset") {
-      print("Using preloaded dataset...")
-      
-      data <- get(input$dataset)
-      
-      if (!is.null(data)) {
-        print("Data is not null...")
-        
-        if (input$var %in% names(data)) {
-          print("Variable found in dataset...")
-          
-          data_column <- data[[input$var]]
-          data <- data[complete.cases(data_column) & is.finite(data_column), ]
-          
-          if (is.numeric(data_column)) {
-            print("Numeric variable selected...")
-            
-            p <- ggplot(data = data, aes_string(x = input$var)) +
-              geom_histogram(mapping = aes(y = ..density.., fill = "Histogram"), color = "black", bins = 30) +
-              geom_density(color = "black", alpha = 0.5, size = 1.5) +
-              labs(x = "Value", y = "Density", title = "Histogram with Density Plot") +
-              geom_vline(mapping = aes(xintercept = mean(data_column), color = "Mean"), size = 2) +
-              geom_vline(mapping = aes(xintercept = median(data_column), color = "Median"), size = 2) +
-              scale_color_manual("", values = c(Mean = "#D55E00", Median = "#0072B2")) +
-              scale_fill_manual("", values = c("#CC79A7"), guide = FALSE) +
-              theme(legend.position = "none")
-            
-            print("Plot created successfully...")
-            
-            return(p)
-          } else {
-            print("Non-numeric variable selected...")
-            # Non-numeric variable selected
-            return(NULL)
-          }
-        } else {
-          print("Variable not found in dataset...")
-          # Dataset or variable not found
-          return(NULL)
-        }
-      } else {
-        print("Data is null...")
-        # Dataset is null
-        return(NULL)
-      }
+      plotOutput("dataset_plot", width = "100%", height = "400px")
     } else {
-      print("No preloaded dataset selected...")
-      # No preloaded dataset selected
-      return(NULL)
+      plotOutput("upload_plot", width = "100%", height = "400px")
     }
   })
   
-  
-  
-  
-  
-  output$dataset_table <- renderTable({
-    req(input$var, input$data_choice, input$dataset)
-    
-    print("Dataset table rendering started...")
-    print(input$dataset)
-    
+  output$data_table <- renderTable({
     if (input$data_choice == "Preloaded Dataset") {
-      print("Using preloaded dataset...")
-      
+      req(input$var, input$data_choice, input$dataset)
       data <- get(input$dataset)
-      
       if (!is.null(data)) {
-        print("Data is not null...")
-        
         if (input$var %in% names(data)) {
-          print("Variable found in dataset...")
-          
           data_column <- data[[input$var]]
           data_summary <- summary(data_column, na.rm = TRUE)
           data_iqr <- IQR(data_column, na.rm = TRUE)
           data_range <- diff(range(data_column, na.rm = TRUE))
-          
           missing_count <- sum(is.na(data_column))
-          
           atable <- data.frame(Summary = c("Min", "Q1", "Mean", "Median", "Q3", "Max", "IQR", "Range","Number of NA Observations"),
                                Values = c(data_summary["Min."], data_summary["1st Qu."], mean(data_column), data_summary["Median"],
                                           data_summary["3rd Qu."], data_summary["Max."],
                                           data_iqr,
                                           data_range,
                                           missing_count))
-          
-          print("Table created successfully...")
-          
           return(atable)
-        } else {
-          print("Variable not found in dataset...")
-          # Dataset or variable not found
-          return(NULL)
         }
-      } else {
-        print("Data is null...")
-        # Dataset is null
-        return(NULL)
       }
     } else {
-      print("No preloaded dataset selected...")
-      # No preloaded dataset selected
-      return(NULL)
+      req(input$var, input$file$datapath)
+      df <- read.csv(input$file$datapath)
+      data_column <- df[[input$var]]
+      data_summary <- summary(data_column, na.rm = TRUE)
+      data_iqr <- IQR(data_column, na.rm = TRUE)
+      data_range <- diff(range(data_column, na.rm = TRUE))
+      missing_count <- sum(is.na(data_column))
+      atable <- data.frame(Summary = c("Min", "Q1", "Mean", "Median", "Q3", "Max", "IQR", "Range","Number of NA Observations"),
+                           Values = c(data_summary["Min."], data_summary["1st Qu."], mean(data_column, na.rm = TRUE), data_summary["Median"],
+                                      data_summary["3rd Qu."], data_summary["Max."],
+                                      data_iqr,
+                                      data_range,
+                                      missing_count))
+      return(atable)
     }
   })
-  
   
   observeEvent(input$file, {
     req(input$file)
     df <- read.csv(input$file$datapath, header = TRUE)
     updateSelectInput(session, "var", choices = names(df))
-    
-    output$upload_plot <- renderPlot({
-      req(input$var)
-      data <- df
-      
-      if (is.numeric(data[[input$var]])) {
-        # Quantitative variable selected
-        data_column <- data[[input$var]]
-        data <- data[complete.cases(data_column) & is.finite(data_column), ]
-        
-        # Create histogram plot
-        p <- ggplot(data = data, aes_string(x = input$var)) +
-          geom_histogram(mapping = aes(y = ..density.., fill = "Histogram"), color = "black", bins = 30) +
-          geom_density(color = "black", alpha = 0.5, size = 1.5) +
-          labs(x = "Value", y = "Density", title = "Histogram with Density Plot") +
-          geom_vline(mapping = aes(xintercept = mean(data_column), color = "Mean"), size = 2) +
-          geom_vline(mapping = aes(xintercept = median(data_column), color = "Median"), size = 2) +
-          scale_color_manual("", values = c(Mean = "#D55E00", Median = "#0072B2")) +
-          scale_fill_manual("", values = c("#CC79A7"), guide = FALSE) +
-          theme(legend.position = "none")
-        
-        return(p)
-      } else {
-        # Non-numeric variable selected
-        return(NULL)
-      }
-    })
-    
-    output$upload_table <- renderTable({
-      req(input$var)
-      data <- df
-      
-      if (is.numeric(data[[input$var]])) {
-        # Quantitative variable selected
-        data_column <- data[[input$var]]
-        data_summary <- summary(data_column, na.rm = TRUE)
-        data_iqr <- IQR(data_column, na.rm = TRUE)
-        data_range <- diff(range(data_column, na.rm = TRUE))
-        
-        missing_count <- sum(is.na(data_column))
-        
-        atable <- data.frame(Summary = c("Min", "Q1", "Mean", "Median", "Q3", "Max", "IQR", "Range","Number of NA Observations"),
-                             Values = c(data_summary["Min."], data_summary["1st Qu."], mean(data_column), data_summary["Median"],
-                                        data_summary["3rd Qu."], data_summary["Max."],
-                                        data_iqr,
-                                        data_range,
-                                        missing_count))
-        
-        return(atable)
-      } else {
-        # Non-numeric variable selected
-        return(NULL)
-      }
-    })
   })
   
+  output$dataset_plot <- renderPlot({
+    req(input$var, input$data_choice, input$dataset)
+    data <- get(input$dataset)
+    if (!is.null(data)) {
+      if (input$var %in% names(data)) {
+        data_column <- data[[input$var]]
+        data <- data[complete.cases(data_column) & is.finite(data_column), ]
+        if (is.numeric(data_column)) {
+         
+          
+          
+          
+          p <-  ggplot(data = data, aes_string(x = input$var)) +
+            geom_histogram(mapping = aes(fill = "Histogram"), color = "black", bins = 30) +
+            labs(x = "Value", y = "Frequency", title = "Histogram") +
+            geom_vline(mapping = aes(xintercept = mean(.data[[input$var]]), color = "Mean"), size = 2) +
+            geom_vline(mapping = aes(xintercept = median(.data[[input$var]]), color = "Median"), size = 2) +
+            scale_color_manual("", values = c(Mean = "#D55E00", Median = "#882255")) +
+            scale_fill_manual("", values = c("#56B4E9"), guide = FALSE) +
+            theme(legend.position = "none")
+          
+          bp <-  ggplot(data = data, aes_string(x = input$var)) +
+            geom_boxplot(color = "black", fill = "#56B4E9") +
+            geom_vline(mapping = aes(xintercept = mean(.data[[input$var]]), color = "Mean"), size = 2) +
+            geom_vline(mapping = aes(xintercept = median(.data[[input$var]]), color = "Median"), size = 2) +
+            scale_color_manual("", values = c(Mean = "#D55E00", Median = "#882255")) +
+            scale_fill_manual("", values = c("#56B4E9"), guide = FALSE) +
+            labs(x = "Value", y = " ", title = "Boxplot") +
+            theme(legend.position = "right")
+          
+          # Set the height of each plot
+          p <- p + theme(plot.margin = margin(0, 0, 0, 0, "cm"))
+          bp <- bp + theme(plot.margin = margin(0, 0, 0, 0, "cm"))
+          
+          # Return the combined plots
+          gridExtra::grid.arrange(p, bp, nrow = 1)
+          
+        } else {
+          return(NULL)
+        }
+      } else {
+        return(NULL)
+      }
+    } else {
+      return(NULL)
+    }
+  })
+  
+  output$upload_plot <- renderPlot({
+    req(input$var, input$file$datapath)
+    df <- read.csv(input$file$datapath)
+    if (is.numeric(df[[input$var]])) {
+      data_column <- df[[input$var]]
+      data <- df[complete.cases(data_column) & is.finite(data_column), ]
+      p <- ggplot(data = data, aes_string(x = input$var)) +
+        geom_histogram(mapping = aes(y = ..density.., fill = "Histogram"), color = "black", bins = 30) +
+        geom_density(color = "black", alpha = 0.5, size = 1.5) +
+        labs(x = "Value", y = "Density", title = "Histogram with Density Plot") +
+        geom_vline(mapping = aes(xintercept = mean(data_column), color = "Mean"), size = 2) +
+        geom_vline(mapping = aes(xintercept = median(data_column), color = "Median"), size = 2) +
+        scale_color_manual("", values = c(Mean = "#D55E00", Median = "#882255")) +
+        scale_fill_manual("", values = c("#56B4E9"), guide = FALSE) +
+        theme(legend.position = "none")
+      bp <-  ggplot(data = data, aes_string(x = input$var)) +
+        geom_boxplot(color = "black", fill = "#56B4E9") +
+        geom_vline(mapping = aes(xintercept = mean(.data[[input$var]]), color = "Mean"), size = 2) +
+        geom_vline(mapping = aes(xintercept = median(.data[[input$var]]), color = "Median"), size = 2) +
+        scale_color_manual("", values = c(Mean = "#D55E00", Median = "#882255")) +
+        scale_fill_manual("", values = c("#56B4E9"), guide = FALSE) +
+        labs(x = "Value", y = " ", title = "Boxplot") +
+        theme(legend.position = "right")
+      
+      # Set the height of each plot
+      p <- p + theme(plot.margin = margin(0, 0, 0, 0, "cm"))
+      bp <- bp + theme(plot.margin = margin(0, 0, 0, 0, "cm"))
+      
+      # Return the combined plots
+      gridExtra::grid.arrange(p, bp, nrow = 1)
+    } else {
+      return(NULL)
+    }
+  })
+
   
   
   
@@ -367,14 +328,15 @@ server <- function(input, output, session) {
   
   output$table <- renderTable({
     data <- summary(df()$value)
-    data_iqr <- IQR(df()$value)
-    data_range <- diff(range(df()$value))
+    data_iqr <- IQR(df()$value, na.rm = TRUE)  # Handle missing values
+    data_range <- diff(range(df()$value, na.rm = TRUE))  # Handle missing values
     
     atable <- data.frame(Summary = c("Min", "Q1", "Mean", "Median", "Q3", "Max", "IQR", "Range"),
-                         Values = c(data["Min."], data["1st Qu."], mean(df()$value), data["Median"], data["3rd Qu."], data["Max."], data_iqr, data_range))
+                         Values = c(data["Min."], data["1st Qu."], mean(df()$value, na.rm = TRUE), data["Median"], data["3rd Qu."], data["Max."], data_iqr, data_range))
     
     return(atable)
   })
+  
   
   output$hist_dynamic <- renderPlot({
     ggplot(data = df_dynamic(), aes(x = value)) +
