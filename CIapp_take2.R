3c563ff4c31e14f54242a619bcf84d09dab2fd72
ninada25 <austrian@lawrence.edu>
2024-05-14 21:36:11 +0000

    comment

--- CIapp_take2.R
+++ CIapp_take2.R
@@ -1,3 +1,10 @@
+################################################################################
+# This app demonstrates the concept of confidence intervals for various contexts,  
+# specifically the ideas such as confidence intervals are not fixed, they might not 
+# contain the "true" population parameter, how CIs are affected by sample sizes, 
+# the interpretation of CIs.
+################################################################################
+
 library(tidyverse)
 library(plotly)
 library(shiny)
